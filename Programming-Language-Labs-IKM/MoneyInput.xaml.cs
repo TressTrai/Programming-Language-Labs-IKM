da2489eda80e6e3d1be2b3a739f1e1d134afcd2a
Anna <a.arlanova@yandex.ru>
2024-01-18 03:45:44 +0500

    add usermoney information and transition to second page

--- Programming-Language-Labs-IKM/MoneyInput.xaml.cs
+++ Programming-Language-Labs-IKM/MoneyInput.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,28 @@ namespace Programming_Language_Labs_IKM
         public MoneyInput()
         {
             InitializeComponent();
+        }
+
+        // Отображаем только цифры при вводе в текст бокс
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        // Сохраняем данные о вводе счета пользователя
+        private void Button_Click_Confirm(object sender, RoutedEventArgs e)
+        {
+            string rubles = RublesTextBox.Text;
+            string kopeeks = KopeeksTextBox.Text;
+
+            // Запись ресурса Money
+            Money UserMoney = new Money(uint.Parse(rubles), byte.Parse(kopeeks));
+            Container.UserMoney = UserMoney;
+
+            // Переход на следущую страницу
+            MoneyOperations nextPage = new MoneyOperations();
+            this.NavigationService.Navigate(nextPage);
         }
     }
 }
--- Programming-Language-Labs-IKM/MoneyOperations.xaml.cs
+++ Programming-Language-Labs-IKM/MoneyOperations.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,11 +24,24 @@ namespace Programming_Language_Labs_IKM
         public MoneyOperations()
         {
             InitializeComponent();
+
+            // При запуске отображаем сколько у нас денег
+            Money UserMoney = Container.UserMoney;
+
+            OutputMoney_Label.Content = "У вас " + UserMoney;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // Отображаем только цифры при вводе в текст бокс
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
+
+        private void IncrementMoney(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
     }
 }
