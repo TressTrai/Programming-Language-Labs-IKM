fd7026cdf9bab54f2b0a48226d9c7385ee183613
Anna <a.arlanova@yandex.ru>
2024-01-18 06:19:54 +0500

    error handling 2nd page

--- Programming-Language-Labs-IKM/MoneyOperations.xaml.cs
+++ Programming-Language-Labs-IKM/MoneyOperations.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -33,7 +34,7 @@ namespace Programming_Language_Labs_IKM
         // Обновление счета
         private void UpdateOutputLabel()
         {
-            OutputMoney_Label.Content = "У вас " + UserMoney;
+            OutputMoney_Label.Content = "Счет " + UserMoney;
         }
 
         // Отображаем только цифры при вводе в текст бокс
@@ -57,28 +58,58 @@ namespace Programming_Language_Labs_IKM
             UpdateOutputLabel();
         }
 
-
+        // Подсчет копеек по кнопке
         private void CountKopeek_Button_Click(object sender, RoutedEventArgs e)
         {
-            UserMoney.AddKopeeks(uint.Parse(KopeekAdd_TextBox.Text));
+            uint kopeeks = 0;
+            
+            if (!uint.TryParse(KopeekAdd_TextBox.Text, out kopeeks))
+            {
+                MessageBox.Show("Проверьте, что ведено число до 4294967295", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            UserMoney.AddKopeeks(kopeeks);
             UpdateOutputLabel();
         }
 
+        // Подсчет рублей по кнопке
         private void CountRubles_Button_Click(object sender, RoutedEventArgs e)
         {
+            uint rubles = 0;
+            
+            if (!uint.TryParse(RublesAdd_TextBox.Text, out rubles))
+            {
+                MessageBox.Show("Проверьте, что ведено число до 4294967295", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // выбран пункт сложения
             if (SumSub_ComboBox.SelectedIndex ==  0 ){
-                if (Order_ComboBox.SelectedIndex == 0)
-                    UserMoney = UserMoney + uint.Parse(RublesAdd_TextBox.Text);
-                else
-                    UserMoney = uint.Parse(RublesAdd_TextBox.Text) + UserMoney;
+                if (Order_ComboBox.SelectedIndex == 0) // левостороняя
+                    UserMoney = UserMoney + rubles;
+                else // правосторняя
+                    UserMoney = rubles + UserMoney;
+
+            // Выбран пункт вычитания
             } else {
-                if (Order_ComboBox.SelectedIndex == 0)
-                    UserMoney = UserMoney - uint.Parse(RublesAdd_TextBox.Text);
-                else
-                    UserMoney = uint.Parse(RublesAdd_TextBox.Text) - UserMoney;
+        
+                if (Order_ComboBox.SelectedIndex == 0) // левостороняя
+                    UserMoney = UserMoney - rubles;
+                else // правостороняя
+                    UserMoney = rubles - UserMoney;
             }
 
             UpdateOutputLabel();
+        }
+
+        // Удаление пробелов с текстбокса
+        private void InputMoneyTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            textBox.Text = textBox.Text.Replace(" ", string.Empty);
+            textBox.SelectionStart = textBox.Text.Length;
+
         }
     }
 }
